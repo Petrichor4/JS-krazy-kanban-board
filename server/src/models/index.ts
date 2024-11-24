641f9a1a5e75aadb263d024b2e98c1f4f208b583
Preston Stone <stonepreston05@gmail.com>
2024-11-23 19:16:14 -0700

    configuring app for deployment

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -8,7 +8,7 @@ import { TicketFactory } from './ticket.js';
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: 'dpg-ct0ll83tq21c73eg291g-a',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
